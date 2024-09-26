30fb99d8c1efcf82088c57fff1a42307eb72d94a
FWNT <fwnt35808319@gmail.com>
2024-09-26 20:44:21 +0800

    settingui

--- app/src/main/java/com/example/billapp/SettingScreen.kt
+++ app/src/main/java/com/example/billapp/SettingScreen.kt
@@ -2,7 +2,9 @@ package com.example.billapp
 
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -22,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -40,6 +43,8 @@ fun SettingScreen(
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color(0xFFE4DFCB))
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -52,118 +57,48 @@ fun SettingScreen(
                 .padding(bottom = 16.dp),
             color = Color.Black
         )
-        Button(onClick = {
-            navController.navigate("profile")
-        },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .size(height = 60.dp, width = 200.dp),
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(Color.LightGray)
-        ) {
-            Text(text = "帳號",
-                fontSize = 18.sp,
-                color = Color.Black)
-        }
-        Button(onClick = {
-            navController.navigate("contact_us")
-        },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .size(height = 60.dp, width = 200.dp),
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(Color.LightGray)
-        ) {
-            Text(text = "聯絡我們",
-                fontSize = 18.sp,
-                color = Color.Black)
-        }
-        Button(
+
+        SettingButton(
+            text = "帳號",
+            icon = painterResource(id = R.drawable.baseline_person_24),
+            onClick = {
+                navController.navigate("profile")
+            }
+        )
+
+        SettingButton(
+            text = "聯絡我們",
+            icon = painterResource(id = R.drawable.baseline_call_24),
+            onClick = {
+                navController.navigate("contact_us")
+            }
+        )
+
+        SettingButton(
+            text = "問題回報",
+            icon = painterResource(id = R.drawable.baseline_report_problem_24),
             onClick = {
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://forms.gle/gi6Fyew6qfFyVAn29"))
                 context.startActivity(intent)
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .size(height = 60.dp, width = 200.dp),
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(Color.LightGray)
-        ) {
-            Text(
-                text = "問題回報",
-                fontSize = 18.sp,
-                color = Color.Black,
-            )
-        }
-        Button(onClick = {
-            navController.navigate("about")
-        },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .size(height = 60.dp, width = 200.dp),
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(Color.LightGray)
-        ) {
-            Text(text = "關於",
-                fontSize = 18.sp,
-                color = Color.Black)
-        }
+            }
+        )
 
-        Button(onClick = {
-            navController.navigate("ItemAdd")
-        },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .size(height = 60.dp, width = 200.dp),
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(Color.LightGray)
-        ) {
-            Text(text = "ItemAdd 暫時按鈕",
-                fontSize = 18.sp,
-                color = Color.Black)
-        }
-        Button(onClick = {
-            navController.navigate("TEST")
-        },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .size(height = 60.dp, width = 200.dp),
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(Color.LightGray)
-        ) {
-            Text(text = "TEST",
-                fontSize = 18.sp,
-                color = Color.Black)
-        }
-
-        Button(onClick = {
-            navController.navigate("avatar")
-        },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .size(height = 60.dp, width = 200.dp),
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(Color.LightGray)
-        ) {
-            Text(text = "AvatarScreen 暫時按鈕",
-                fontSize = 18.sp,
-                color = Color.Black)
-        }
-
+        SettingButton(
+            text = "關於",
+            icon = painterResource(id = R.drawable.baseline_speaker_notes_24),
+            onClick = {
+                navController.navigate("about")
+            }
+        )
     }
-
-
 }
 
 @Composable
-fun SettingButton(text: String, onClick: () -> Unit) {
+fun SettingButton(
+    text: String,
+    icon: Painter,
+    onClick: () -> Unit
+) {
     Button(
         onClick = onClick,
         modifier = Modifier
@@ -171,13 +106,19 @@ fun SettingButton(text: String, onClick: () -> Unit) {
             .padding(vertical = 8.dp)
             .size(height = 60.dp, width = 200.dp), // 設定按鈕高度和寬度
         shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.buttonColors(Color.LightGray)
+        colors = ButtonDefaults.buttonColors(Color(0xFFBCBCBC))
     ) {
+        Icon(
+            painter = icon,
+            contentDescription = null,
+            modifier = Modifier.size(24.dp),
+            tint = Color.Black
+        )
         Text(
             text = text,
             fontSize = 18.sp,
-            color = Color.Black
+            color = Color.Black,
+            modifier = Modifier.padding(start = 8.dp)
         )
-
     }
 }
