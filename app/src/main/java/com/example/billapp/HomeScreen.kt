ab56cef57b9013250740771c8fddd2a2d8187113
Jason Lin <jasonlin930309@gmail.com>
2024-09-30 20:42:29 +0800

    主頁可滑動、交易顏色改成淺黃

--- app/src/main/java/com/example/billapp/HomeScreen.kt
+++ app/src/main/java/com/example/billapp/HomeScreen.kt
@@ -2,36 +2,41 @@ package com.example.billapp
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
 import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.IconButton //
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -45,11 +50,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -63,34 +67,20 @@ import com.example.billapp.models.Group
 import com.example.billapp.models.PersonalTransaction
 import com.example.billapp.models.User
 import com.example.billapp.ui.theme.BoxBackgroundColor
-import com.example.billapp.ui.theme.Brown2
-import com.example.billapp.ui.theme.MainBackgroundColor
-import com.example.billapp.ui.theme.MainCardRedColor
 import com.example.billapp.ui.theme.Brown1
 import com.example.billapp.ui.theme.Brown2
-import com.example.billapp.ui.theme.Red
-import com.example.billapp.ui.theme.Green
-import com.example.billapp.viewModel.AvatarViewModel
-import com.example.billapp.viewModel.MainViewModel
-import java.util.Calendar
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
-import com.example.billapp.ui.theme.Gray
-import com.example.billapp.ui.theme.VeryDarkGray
-import androidx.compose.material.icons.filled.ArrowBack as ArrowB
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
-import androidx.compose.ui.draw.shadow
-import com.example.billapp.ui.theme.Brown4
 import com.example.billapp.ui.theme.DarkerGray
-import com.example.billapp.ui.theme.HightlightWhiteColor
+import com.example.billapp.ui.theme.Green
+import com.example.billapp.ui.theme.MainBackgroundColor
+import com.example.billapp.ui.theme.MainCardRedColor
 import com.example.billapp.ui.theme.PieGreenColor
 import com.example.billapp.ui.theme.PieRedColor
-import kotlinx.coroutines.launch
+import com.example.billapp.ui.theme.Red
+import com.example.billapp.ui.theme.VeryDarkGray
+import com.example.billapp.viewModel.AvatarViewModel
+import com.example.billapp.viewModel.MainViewModel
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Locale
 
 
@@ -222,6 +212,7 @@ fun HomeScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(MainBackgroundColor)
+            .verticalScroll(rememberScrollState())
     ) {
 
         val income = filteredIncome
@@ -524,8 +515,6 @@ fun BusinessCardBack(income: Float, expense: Float, balance: Float, total: Float
             drawRect(Brown1)
         }
 
-
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -663,10 +652,10 @@ fun TransactionItem(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(70.dp)
-            .background(color = BoxBackgroundColor),
+            .height(70.dp),
         elevation = CardDefaults.cardElevation(4.dp),
-        shape = RoundedCornerShape(8.dp)
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFFFF8D8))
     ) {
         Row(
             modifier = Modifier
