9eb2688ca97bcb6ba8bb6aef81267c0b35501bee
rickypaul666 <paulrin2500@gmail.com>
2024-09-30 23:17:35 +0800

    UI update

--- app/src/main/java/com/example/billapp/GroupTest.kt
+++ app/src/main/java/com/example/billapp/GroupTest.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
